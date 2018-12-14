e770aff8d40f50de45d7fa26b3c42ee1ce1624c7
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-12-14 21:40:00 +0100

    Integrated PID in main; manually-tuned parameters work well

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,10 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid;
   // TODO: Initialize the pid variable.
+
+  PID pid;
+  pid.Init(0.5, 0., 20.);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -50,13 +52,15 @@ int main()
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
+          pid.UpdateError(cte);
+          double steer_value = pid.SteeringAngle();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
