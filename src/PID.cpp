10137e5a7901bab5783f64fac66cede4ba86a4b8
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-12-14 11:18:45 +0100

    Started working on the PID class

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,18 @@
 #include "PID.h"
 
-using namespace std;
-
-/*
-* TODO: Complete the PID class.
-*/
-
-PID::PID() {}
-
-PID::~PID() {}
-
 void PID::Init(double Kp, double Ki, double Kd) {
+
+  Kp_ = Kp;
+  Ki_ = Ki;
+  Kd_ = Kd;
+
 }
 
 void PID::UpdateError(double cte) {
+
 }
 
 double PID::TotalError() {
+
 }
 
--- src/PID.h
+++ src/PID.h
@@ -4,42 +4,42 @@
 class PID {
 public:
   /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
+   * Errors
+   */
+  double p_error_;
+  double i_error_;
+  double d_error_;
 
   /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
+   * Coefficients
+   */
+  double Kp_;
+  double Ki_;
+  double Kd_;
 
   /*
-  * Constructor
-  */
-  PID();
+   * Constructor
+   */
+  PID() : p_error_{0}, i_error_{0}, d_error_{0}, Kp_{0}, Ki_{0}, Kd_{0} {};
 
   /*
-  * Destructor.
-  */
-  virtual ~PID();
+   * Destructor.
+   */
+  virtual ~PID() = default;
 
   /*
-  * Initialize PID.
-  */
+   * Initialize PID.
+   */
   void Init(double Kp, double Ki, double Kd);
 
   /*
-  * Update the PID error variables given cross track error.
-  */
+   * Update the PID error variables given cross track error.
+   */
   void UpdateError(double cte);
 
   /*
-  * Calculate the total PID error.
-  */
+   * Calculate the total PID error.
+   */
   double TotalError();
 };
 
